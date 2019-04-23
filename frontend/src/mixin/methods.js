b48491a2578ef7e8310cad1853d96d2d11549b3c
Fritz Larco <flarco@flarco-ltm.internal.salesforce.com>
2019-04-23 17:34:27 -0400

    add limit for redshift

--- frontend/src/mixin/methods.js
+++ frontend/src/mixin/methods.js
@@ -1553,9 +1553,11 @@ var methods = {
   },
 
   get_object_data(object_full_name, tab_id) {
+    let sql = "select * from " + object_full_name
+    if(this.curr_database_type.toLowerCase() == 'redshift') sql = sql + ' limit 5000' 
     let query = new classes.SqlQuery({
       database: this.$store.query.db_name,
-      sql: "select * from " + object_full_name,
+      sql: sql,
       limit: this.$store.query._session.tabs[tab_id].limit
     });
 
